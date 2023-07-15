2804dd291b73270e92d66acc9af918cf564083b4
c01952f666540050efb7f2750d8e3e91 <felix.voo@ledger.com>
2023-07-15 22:52:35 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ledger Recover Self-Implementation
+
+For a better understanding of Ledger Recover, I've attempted to implement it within python.
+Please note that this is not intended for any real-world keys or for use with any real private keys. 
+It is for educational purposes only.
---
+++ mnemonic.py
@@ -0,0 +1,56 @@
+# IMPORTS
+import hashlib
+import binascii
+import secrets
+from unicodedata import normalize
+import base64
+import base58
+
+# Recovery Phrase Generation
+def generate_recovery_phrase(bits:int=256, iterations:int=256, key_bytes:int=64, hash_name:str='sha512', passphrase:str="") -> str:
+
+    with open("wordlist/english.txt", 'r') as file:
+        wordlist = file.read().splitlines()
+
+    # Check that the number of bits is divisible by 32
+    if bits % 32 != 0:
+        raise ValueError("The number of bits must be divisible by 32")
+
+    # Generate a random number
+    entropy = secrets.token_bytes(bits // 8)
+
+    # Calculate the checksum
+    checksum = hashlib.sha256(entropy).digest()
+
+    # Add the checksum to the entropy
+    bits = bin(int(binascii.hexlify(entropy + checksum[:(bits // 32) // 8]), 16))[2:].zfill(bits + bits // 32)
+
+    # Convert to mnemonic
+    phrase = []
+    for i in range(len(bits) // 11):
+        idx = int(bits[i * 11:(i + 1) * 11], 2)
+        phrase.append(wordlist[idx])
+    
+    salt = str(phrase) + normalize('NFKD', passphrase)
+    seed = hashlib.pbkdf2_hmac(hash_name, normalize('NFKD', " ".join(phrase)).encode(), salt.encode(), iterations, key_bytes)
+
+
+    return {
+        "phrase": " ".join(phrase),
+        "entropy": entropy,
+        "entropy_hex": binascii.hexlify(entropy).decode(),
+        "entropy_base64": base64.b64encode(entropy).decode(),
+        "entropy_base58": base58.b58encode(entropy).decode(),
+        "entropy_int": int.from_bytes(entropy, 'big'),
+        "seed": seed,
+        "seed_hex": binascii.hexlify(seed).decode(),
+        "seed_base64": base64.b64encode(seed).decode(),
+        "seed_base58": base58.b58encode(seed).decode(),
+        "seed_int": int.from_bytes(seed, 'big'),
+    }
+
+test_phrase = generate_recovery_phrase()
+
+for x in test_phrase.keys():
+    print(f"{x} // {test_phrase[x]}")
+
